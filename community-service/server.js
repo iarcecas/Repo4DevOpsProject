4d4de21023b79dbc21295c15dbbb718c24b9cf20
Ignacio Arce <iarcecas@my.centennialcollege.ca>
2025-04-10 18:28:26 -0400

    feat(community-service): Setup base Express project structure AB#169
    chore: Update pipeline to install community-service deps

---
+++ community-service/server.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import cors from 'cors';
+import http from 'http';
+import connectDB from './config/db.js';
+import typeDefs from './graphql/typeDefs.js';
+import resolvers from './graphql/resolvers.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const customResolvers = {
+  ...resolvers,
+};
+
+async function startServer() {
+  const app = express();
+  const httpServer = http.createServer(app);
+
+  connectDB();
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers: customResolvers,
+  });
+
+  await server.start();
+
+  app.use(cors());
+  app.use(express.json());
+  app.use('/graphql', expressMiddleware(server));
+
+  const PORT = process.env.PORT || 4002;
+  httpServer.listen(PORT, () => {
+    console.log(`Community Service running on http://localhost:${PORT}/graphql`);
+  });
+}
+
+startServer();
