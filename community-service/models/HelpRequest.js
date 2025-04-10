094a50ce46174944ae898cc00e590f364fe23084
Ignacio Arce <iarcecas@my.centennialcollege.ca>
2025-04-10 18:35:00 -0400

    feat(community-service): Add MongoDB schemas and DB config AB#174

---
+++ community-service/models/HelpRequest.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const HelpRequestSchema = new Schema({
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: false,
+  },
+  isResolved: {
+    type: Boolean,
+    default: false,
+  },
+  volunteers: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+HelpRequestSchema.pre('save', function(next) {
+  this.updatedAt = Date.now();
+  next();
+});
+
+export default mongoose.model('HelpRequest', HelpRequestSchema);
